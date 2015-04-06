81d398ed26aa775e8646382e579094fd678e8145
Marcin Czołnowski <marcin@czolnowski.net>
2015-04-06 18:10:13 +0200

    Add cache implementation.

---
+++ src/Mindweb/TrackerKernelStandard/Configuration/Cache.php
@@ -0,0 +1,29 @@
+<?php
+namespace Mindweb\TrackerKernelStandard\Configuration;
+
+use Mindweb\TrackerKernel as Adapter;
+use SplFileInfo;
+
+class Cache implements Adapter\Configuration\Cache
+{
+    /**
+     * @var SplFileInfo
+     */
+    private $fileInfo;
+
+    /**
+     * @param SplFileInfo $fileInfo
+     */
+    public function __construct(SplFileInfo $fileInfo)
+    {
+        $this->fileInfo = $fileInfo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->fileInfo->getPath();
+    }
+}
